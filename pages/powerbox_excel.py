d4da315615cceb5f482ef62ddd4e444c3b140d6d
syl3sh <sylesh2206@gmail.com>
2026-07-28 15:55:31 +0800

    Update powerbox_excel.py

--- pages/powerbox_excel.py
+++ pages/powerbox_excel.py
@@ -99,6 +99,14 @@ if not versions:
 latest_version = versions[0]
 
 current_df = download_version(latest_version)
+new_col_name = st.text_input("new column name")
+if st.button("Add column"):
+    if new_col_name and new_col_name not in current_df.columns:
+        current_df[new_col_name] = ""
+        st.success(f"Added column: {new_col_name}")
+        st.rerun()
+    elif new_col_name in current_df.columns:
+        st.warning("A column with that name already exists")
 
 edited_df=st.data_editor(current_df, num_rows="dynamic")
 
