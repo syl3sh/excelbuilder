db96cc2929a1e56ecaa65b3005649b1ecec4804f
syl3sh <sylesh2206@gmail.com>
2026-07-28 11:11:47 +0800

    Update powerbox_excel.py

--- pages/powerbox_excel.py
+++ pages/powerbox_excel.py
@@ -73,7 +73,7 @@ def save_version(df):
         df.to_excel(writer, index=False, sheet_name="Sheet1")
     buffer.seek(0)
     repo.create_file(
-        path=history_path,
+        path=repo,
         message=f"Save version {timestamp}",
         content=buffer.getvalue(),
         branch= branch
