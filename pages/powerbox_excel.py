a33dde2fb0854c4c1a765ab29b8f96ba09402140
syl3sh <sylesh2206@gmail.com>
2026-08-19 15:36:08 +0800

    Update powerbox_excel.py

--- pages/powerbox_excel.py
+++ pages/powerbox_excel.py
@@ -9,7 +9,7 @@ from streamlit_gsheets import GSheetsConnection
 from github import Github
 
 sgt = pytz.timezone("Asia/Singapore")
-df = pd.read_excel('excelbuilder/ATE_Tracking_Record _10726.xlsx')
+df = pd.read_excel('syl3sh/excelfiles/excelbuilder/ATE_Tracking_Record _10726.xlsx')
 history_path = "excelbuilder/data/history"
 
 
